48c6963c59aadafcd1027ca4995802040b4c877f
Mario Gutierrez <mario.ag188@gmail.com>
2026-03-31 10:26:17 -0500

    learn how to use timer for cooldown

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    private float redColorDuration = 2;
+    public float timer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        timer = timer - Time.deltaTime;
+        
+        if(timer < 0 && spriteRenderer.color != Color.white)
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + "took damage");
+        spriteRenderer.color = Color.red;
+        timer = redColorDuration;
+    }
+
+    private void TurnWhite()
+    {
+        spriteRenderer.color = Color.white;
     }
 }
